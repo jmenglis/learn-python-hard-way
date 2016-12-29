8d7a99feb6d4e4d330ab8786dca89ac5c9a6381c
Josh English <josh@joshenglish.com>
2016-12-28 22:18:13 -0600

    Python refresher go

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, first, second, third = argv
+
+my_life = raw_input("Type something here: ")
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+print "You typed this in:", my_life
